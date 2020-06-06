78df4b8270ed188b1a2c03f4a62bd9372071fd24
Saffian-Asghar <66506525+Saffian-Asghar@users.noreply.github.com>
2020-06-06 11:06:27 +0500

    Add files via upload

---
+++ lanes.py
@@ -0,0 +1,96 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+def make_points(image, line):
+    slope, intercept = line
+    y1 = int(image.shape[0])# bottom of the image
+    y2 = int(y1*3/5)         # slightly lower than the middle
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    return [[x1, y1, x2, y2]]
+
+def average_slope_intercept(image, lines):
+    left_fit    = []
+    right_fit   = []
+    if lines is None:
+        return None
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            fit = np.polyfit((x1,x2), (y1,y2), 1)
+            slope = fit[0]
+            intercept = fit[1]
+            if slope < 0: # y is reversed in image
+                left_fit.append((slope, intercept))
+            else:
+                right_fit.append((slope, intercept))
+    # add more weight to longer lines
+    left_fit_average  = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    left_line  = make_points(image, left_fit_average)
+    right_line = make_points(image, right_fit_average)
+    averaged_lines = [left_line, right_line]
+    return averaged_lines
+
+def canny(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    kernel = 5
+    blur = cv2.GaussianBlur(gray,(kernel, kernel),0)
+    canny = cv2.Canny(gray, 100, 240)
+    return canny
+
+def display_lines(img,lines):
+    line_image = np.zeros_like(img)
+    if lines is not None:
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                cv2.line(line_image,(x1,y1),(x2,y2),(0,0,255),10)
+    return line_image
+
+def region_of_interest(canny):
+    height = canny.shape[0]
+    width = canny.shape[1]
+    mask = np.zeros_like(canny)
+
+    triangle = np.array([[
+    (0, 499.9),
+    (800, 499.9),
+    (353.4, 120.86),
+    (258.3,125.9),
+    (0,247.38)]], np.int32)
+
+    cv2.fillPoly(mask, triangle, 255)
+    masked_image = cv2.bitwise_and(canny, mask)
+    return masked_image
+
+
+image = cv2.imread('img/test_image.jpg')
+lane_image = np.copy(image)
+lane_canny = canny(lane_image)
+cropped_canny = region_of_interest(lane_canny)
+lines = cv2.HoughLinesP(cropped_canny, 2, np.pi/180, 10, np.array([]), minLineLength=40,maxLineGap=5)
+#averaged_lines = average_slope_intercept(image, lines)
+line_image = display_lines(lane_image, lines)
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 0)
+
+cv2.imwrite("img/combo_image.jpg",lines)
+cv2.imshow("img/combo_image",lines)
+cv2.waitKey(0)
+'''
+writer  = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M','J','P','G'), 30.0, (848,480))
+
+cap = cv2.VideoCapture("uetlane.mp4")
+while(cap.isOpened()):
+    _, frame = cap.read()
+    canny_image = canny(frame)
+    cropped_canny = region_of_interest(canny_image)
+    lines = cv2.HoughLinesP(cropped_canny, 2, np.pi/180, 100, np.array([]), minLineLength=40,maxLineGap=5)
+    #averaged_lines = average_slope_intercept(frame, lines)
+    line_image = display_lines(frame, lines)
+    combo_image = cv2.addWeighted(frame, 0.6, line_image, 1, 1)
+    cv2.imshow("result", combo_image)
+    writer.write(combo_image)
+    if cv2.waitKey(33) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+'''
